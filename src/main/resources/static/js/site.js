82c34c53dc07bb69a99e4b02e4c35d32e24d166d
Police Hasini <policehasini1806@gmail.com>
2026-05-25 23:00:24 +0530

    this is the toggle

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,1 +1,18 @@
 console.log("Hello from JS");
+
+var users = [
+    {"name" : "John", "gender" : "Male", "img" : "john_img.png"},
+    {"name" : "Jane", "gender" : "Female", "img" : "jane_img.png"}
+];
+
+var currentUserIndex = 0;
+
+function toggleCard(){
+    currentUserIndex =(currentUserIndex + 1) % 2;
+    var user = users[currentUserIndex];
+
+    // DOM Manipulation
+    document.getElementById("userImage").src = user.img;
+    document.getElementById("userGender").innerHTML = user.gender;
+    document.getElementById("userName").innerHTML = user.name;
+}
